59a06939e7e3cd869400adccd743138f735a053a
AAA <ABC@gmail.com>
2023-03-21 22:36:54 +0900

    adding sample

---
+++ BackgroundWorkerSampleCSharp/BackgroundWorkerSampleCSharp/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BackgroundWorkerSampleCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(393, 160);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(20, 21);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(108, 29);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(146, 21);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(108, 29);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 262);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ BackgroundWorkerSampleCSharp/BackgroundWorkerSampleCSharp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace BackgroundWorkerSampleCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            // Start BackgroundWorker
+            backgroundWorker1.RunWorkerAsync(2000);
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            // Cancel BackgroundWorker
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.CancelAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker helperBW = sender as BackgroundWorker;
+            int arg = (int)e.Argument;
+            e.Result = BackgroundProcessLogicMethod(helperBW, arg);
+            if (helperBW.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        // Put all of background logic that is taking too much time
+        private int BackgroundProcessLogicMethod(BackgroundWorker bw, int a)
+        {
+            int result = 0;
+            Thread.Sleep(5000);
+            //MessageBox.Show("I was doing some work in the background.");
+            Console.WriteLine("I was doing some work in the background.");
+            return result;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //if (e.Cancelled) MessageBox.Show("Operation was canceled");
+            if (e.Cancelled) Console.WriteLine("Operation was canceled");
+            else if (e.Error != null)
+            {
+                Console.WriteLine("error : " + e.Error.Message);
+            }
+            //MessageBox.Show(e.Error.Message);
+            else
+            {
+                Console.WriteLine("result : " + e.Result.ToString());
+                //MessageBox.Show(e.Result.ToString());
+            }
+        }
+    }
+}
